46d87ce583d8e262bf3ac813b6169a6db8578d85
Gitiauxx <xavier.gitiaux@colorado.edu>
2018-11-15 16:29:34 -0700

    Start auditing procedure when oracle access to classifier is allowed

--- scripts/audit_oracle.py
+++ scripts/audit_oracle.py
@@ -18,7 +18,7 @@ def oracle_access(data, features, learner, groups):
     learner: a sklearn object with a predict attribute
             learner called by the oracle
 
-    group: dictionary
+    groups: dictionary
             keys are protected attribute (e.g. gender)
             values are protected values (e.g. male, female)
 
@@ -33,8 +33,85 @@ def oracle_access(data, features, learner, groups):
         test = data[features]
         test['group'] = varvalue
         predict = learner.predict(np.array(test))
-        data[ 'predict_{}_{}'.format(varname, varvalue)]
+        data['predict_{}_{}'.format(varname, varvalue)]
 
     return data
 
-def 
+def binary_audit(data, features, features_audit, audited, 
+                auditor, groups, seed=1):
+    """"
+    This function return a measure of fairness violation
+    when the protected values are binary (e.g. male vs female)
+    and oracle access to the audited learner. The unfairness metric
+    is computed using an auditor learner. The labels are data.difference, the absolute
+    difference between the target values from the audited learner
+    for each protected group
+
+    Parameters:
+    ----------
+    data: pandas table 
+            dataframe with values for each feature
+
+    features: list
+            features used to train audited learner
+
+    features_audit: list
+            features used to train auditor learner
+
+    audited: an object with a predict and fit attribute
+            learner called by the oracle
+    
+    auditor: an object with a predict and fit attribute
+            learner used to audit the audited learner
+            Labels are data[difference]
+
+    groups: dictionary
+            keys are protected attribute (e.g. gender)
+            values are protected values (e.g. male, female)
+            only binary choices among protected values
+
+    Return:
+    ----------
+    results: dictionary
+        keys are protected attribute (e.g. gender)
+        value is the accuracy of auditor when predicting data['difference']
+        for the protected attribute  
+
+    """"
+    data = oracle_access(data, features, audited, groups)
+
+    results = {}
+
+    for varname, varvalue in groups.keys():
+        
+        protected_1 = groups[varname][0]
+        protected2 = groups[varname][1]
+
+        data['difference'] = np.absolute(data['predict_{}_{}'.format(varname, protected_1)] - \
+                            data['predict_{}_{}'.format(varname, protected_2)]
+
+        #split the data into train/test using a 0.7/0.3 ratio
+        train = data.loc[np.random.choice(data.index, int(len(data)* 0.7), replace=True), :]
+        test = data.drop(train.index)
+
+        train_x = np.array(train[features_audit])
+        train_y = np.array(train['difference']).ravel()
+
+        # train auditor
+        auditor.fit(train_x, train_y)
+
+        # predict on test set
+        test_x = np.array(test[features_audit])
+        test_y = np.array(test['difference'])
+        predicted = auditor.predict()
+
+        # compute accuracy
+        accuracy = (predicted == train_y).astype('int32')
+        result[varname] = accuracy
+    
+    return results
+
+
+        
+
+
